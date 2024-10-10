c75385fcaa361901463e153098634adf8f506014
Isabella Fishman <fishman.bellam@gmail.com>
2024-10-10 00:12:51 -0500

    Call API for accurate schedule

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,43 @@
 import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useJsonQuery } from './utilities/fetch';
 import './App.css';
 import Banner from './components/Banner';
 import CourseList from './components/CourseList';
 
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": {
-    "F101" : {
-      "term": "Fall",
-      "number": "101",
-      "meets" : "MWF 11:00-11:50",
-      "title" : "Computer Science: Concepts, Philosophy, and Connections"
-    },
-    "F110" : {
-      "term": "Fall",
-      "number": "110",
-      "meets" : "MWF 10:00-10:50",
-      "title" : "Intro Programming for non-majors"
-    },
-    "S313" : {
-      "term": "Spring",
-      "number": "313",
-      "meets" : "TuTh 15:30-16:50",
-      "title" : "Tangible Interaction Design and Learning"
-    },
-    "S314" : {
-      "term": "Spring",
-      "number": "314",
-      "meets" : "TuTh 9:30-10:50",
-      "title" : "Tech & Human Interaction"
-    }
-  }
-};
+const Main = () => {
+  const [data, isLoading, error] = useJsonQuery('https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php');
 
-const App = () => {
-  
+  if (error) return <h1>Error loading data: {`${error}`}</h1>;
+  if (isLoading) return <h1>Loading data...</h1>;
+  if (!data) return <h1>No data found</h1>;
+
   return (
     <div>
-      <Banner 
-        title={schedule.title}/>
+      <Banner title={data.title} />
       <div className="cards-list">
-        {Object.keys(schedule.courses).map(courseId => 
-          (<CourseList 
+        {Object.keys(data.courses).map(courseId => (
+          <CourseList 
             coursekey={courseId} 
-            course={schedule.courses[courseId]}/>))}
+            course={data.courses[courseId]} 
+          />
+        ))}
       </div>
     </div>
+  );
+}
+
+const queryClient = new QueryClient();
+
+
+const App = () => {
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="container">
+        <Main />
+      </div>
+    </QueryClientProvider>
   );
 };
 
